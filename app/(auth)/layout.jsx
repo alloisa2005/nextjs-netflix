40427f90e22ac5d7d8f2f7626ca809fd7593d0a2
Anthony Allois <anthonyallois@gmail.com>
2024-02-05 10:09:27 -0300

    Ajustes

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AuthLayout = ({ children }) => {
+  return (
+    <div>
+      <p>AuthLayout</p>
+
+      {children}
+      
+    </div>
+  )
+}
+
+export default AuthLayout
