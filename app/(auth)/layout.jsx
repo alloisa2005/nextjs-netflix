2360cda5440847b48b4a4cd4fe245ce88e630cf6
Anthony Allois <anthonyallois@gmail.com>
2024-02-05 10:22:53 -0300

    Ajustes Auth

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import BackgroundImage from "../../public/login_background.jpg";
+import Logo from "../../public/netflix_logo.svg";
 
 const AuthLayout = ({ children }) => {
   return (
-    <div>
-      <p>AuthLayout</p>
+    <div className="relative flex flex-col h-screen w-screen bg-black md:items-center md:justify-center md:bg-transparent">
+      <Image
+        src={BackgroundImage}
+        alt="Background Image"
+        className="hidden sm:flex object-cover -z-10 brightness-50"
+        priority
+        fill
+      />
 
+      <Image
+        src={Logo}
+        alt="Logo"
+        width={120}
+        height={120}
+        priority
+        className="absolute left-4 top-4 object-contain md:left-10 md:top-6"
+      />
       {children}
-      
     </div>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
